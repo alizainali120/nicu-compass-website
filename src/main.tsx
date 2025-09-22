c486e2f04b9bdc8d922fecfcfcd3fc9cc766176e
Ali Zain Ali <alizainali120@gmail.com>
2025-09-21 22:13:03 -0500

    fix issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import './style.css'
 import App from './App'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
+<React.StrictMode>
   <App />
+</React.StrictMode>,
 )
